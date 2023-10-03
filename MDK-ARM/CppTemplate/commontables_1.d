8061fc19a630c4a276c32872488c1902285d1c45
ccbccbccb11 <2942523149@qq.com>
2023-10-03 19:01:42 +0800

    feat: 将DSP 1.15 添加进工程文件

--- MDK-ARM/CppTemplate/commontables_1.d
+++ MDK-ARM/CppTemplate/commontables_1.d
@@ -1,6 +1,6 @@
-./cpptemplate/commontables.o: \
-  ..\Drivers\CMSIS\DSP\Source\CommonTables\CommonTables.c \
-  ..\Drivers\CMSIS\DSP\Source\CommonTables\arm_common_tables.c \
+./cpptemplate/commontables_1.o: \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\CommonTables\CommonTables.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\CommonTables\arm_common_tables.c \
   ..\Drivers\CMSIS\DSP\Include\arm_math_types.h \
   ..\Drivers\CMSIS\Include\cmsis_compiler.h \
   C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
@@ -17,8 +17,8 @@
   ..\Drivers\CMSIS\DSP\Include\dsp\none.h \
   ..\Drivers\CMSIS\DSP\Include\dsp\utils.h \
   ..\Drivers\CMSIS\DSP\Include\dsp\basic_math_functions.h \
-  ..\Drivers\CMSIS\DSP\Source\CommonTables\arm_const_structs.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\CommonTables\arm_const_structs.c \
   ..\Drivers\CMSIS\DSP\Include\arm_const_structs.h \
   ..\Drivers\CMSIS\DSP\Include\dsp\transform_functions.h \
   ..\Drivers\CMSIS\DSP\Include\dsp\complex_math_functions.h \
-  ..\Drivers\CMSIS\DSP\Source\CommonTables\arm_mve_tables.c
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\CommonTables\arm_mve_tables.c
--- MDK-ARM/CppTemplate/complexmathfunctions.d
+++ MDK-ARM/CppTemplate/complexmathfunctions.d
@@ -1,6 +1,6 @@
 ./cpptemplate/complexmathfunctions.o: \
-  ..\Drivers\CMSIS\DSP\Source\ComplexMathFunctions\ComplexMathFunctions.c \
-  ..\Drivers\CMSIS\DSP\Source\ComplexMathFunctions\arm_cmplx_conj_f32.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\ComplexMathFunctions\ComplexMathFunctions.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\ComplexMathFunctions\arm_cmplx_conj_f32.c \
   ..\Drivers\CMSIS\DSP\Include\dsp\complex_math_functions.h \
   ..\Drivers\CMSIS\DSP\Include\arm_math_types.h \
   ..\Drivers\CMSIS\Include\cmsis_compiler.h \
@@ -17,24 +17,24 @@
   ..\Drivers\CMSIS\DSP\Include\dsp\utils.h \
   ..\Drivers\CMSIS\DSP\Include\dsp\fast_math_functions.h \
   ..\Drivers\CMSIS\DSP\Include\dsp\basic_math_functions.h \
-  ..\Drivers\CMSIS\DSP\Source\ComplexMathFunctions\arm_cmplx_conj_q15.c \
-  ..\Drivers\CMSIS\DSP\Source\ComplexMathFunctions\arm_cmplx_conj_q31.c \
-  ..\Drivers\CMSIS\DSP\Source\ComplexMathFunctions\arm_cmplx_dot_prod_f32.c \
-  ..\Drivers\CMSIS\DSP\Source\ComplexMathFunctions\arm_cmplx_dot_prod_q15.c \
-  ..\Drivers\CMSIS\DSP\Source\ComplexMathFunctions\arm_cmplx_dot_prod_q31.c \
-  ..\Drivers\CMSIS\DSP\Source\ComplexMathFunctions\arm_cmplx_mag_f32.c \
-  ..\Drivers\CMSIS\DSP\Source\ComplexMathFunctions\arm_cmplx_mag_f64.c \
-  ..\Drivers\CMSIS\DSP\Source\ComplexMathFunctions\arm_cmplx_mag_q15.c \
-  ..\Drivers\CMSIS\DSP\Source\ComplexMathFunctions\arm_cmplx_mag_fast_q15.c \
-  ..\Drivers\CMSIS\DSP\Source\ComplexMathFunctions\arm_cmplx_mag_q31.c \
-  ..\Drivers\CMSIS\DSP\Source\ComplexMathFunctions\arm_cmplx_mag_squared_f32.c \
-  ..\Drivers\CMSIS\DSP\Source\ComplexMathFunctions\arm_cmplx_mag_squared_f64.c \
-  ..\Drivers\CMSIS\DSP\Source\ComplexMathFunctions\arm_cmplx_mag_squared_q15.c \
-  ..\Drivers\CMSIS\DSP\Source\ComplexMathFunctions\arm_cmplx_mag_squared_q31.c \
-  ..\Drivers\CMSIS\DSP\Source\ComplexMathFunctions\arm_cmplx_mult_cmplx_f32.c \
-  ..\Drivers\CMSIS\DSP\Source\ComplexMathFunctions\arm_cmplx_mult_cmplx_f64.c \
-  ..\Drivers\CMSIS\DSP\Source\ComplexMathFunctions\arm_cmplx_mult_cmplx_q15.c \
-  ..\Drivers\CMSIS\DSP\Source\ComplexMathFunctions\arm_cmplx_mult_cmplx_q31.c \
-  ..\Drivers\CMSIS\DSP\Source\ComplexMathFunctions\arm_cmplx_mult_real_f32.c \
-  ..\Drivers\CMSIS\DSP\Source\ComplexMathFunctions\arm_cmplx_mult_real_q15.c \
-  ..\Drivers\CMSIS\DSP\Source\ComplexMathFunctions\arm_cmplx_mult_real_q31.c
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\ComplexMathFunctions\arm_cmplx_conj_q15.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\ComplexMathFunctions\arm_cmplx_conj_q31.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\ComplexMathFunctions\arm_cmplx_dot_prod_f32.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\ComplexMathFunctions\arm_cmplx_dot_prod_q15.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\ComplexMathFunctions\arm_cmplx_dot_prod_q31.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\ComplexMathFunctions\arm_cmplx_mag_f32.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\ComplexMathFunctions\arm_cmplx_mag_f64.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\ComplexMathFunctions\arm_cmplx_mag_q15.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\ComplexMathFunctions\arm_cmplx_mag_fast_q15.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\ComplexMathFunctions\arm_cmplx_mag_q31.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\ComplexMathFunctions\arm_cmplx_mag_squared_f32.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\ComplexMathFunctions\arm_cmplx_mag_squared_f64.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\ComplexMathFunctions\arm_cmplx_mag_squared_q15.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\ComplexMathFunctions\arm_cmplx_mag_squared_q31.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\ComplexMathFunctions\arm_cmplx_mult_cmplx_f32.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\ComplexMathFunctions\arm_cmplx_mult_cmplx_f64.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\ComplexMathFunctions\arm_cmplx_mult_cmplx_q15.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\ComplexMathFunctions\arm_cmplx_mult_cmplx_q31.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\ComplexMathFunctions\arm_cmplx_mult_real_f32.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\ComplexMathFunctions\arm_cmplx_mult_real_q15.c \
+  C:\Users\29425\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.15.0\Source\ComplexMathFunctions\arm_cmplx_mult_real_q31.c
